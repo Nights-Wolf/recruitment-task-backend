5588f91680a47f02539fa4cbcb9d48bb65d15b9a
Nights-Wolf <dawinavi@gmail.com>
2022-12-14 20:19:41 +0100

    Data number model added.

---
+++ src/main/java/com/recruitment/recruitment_task/models/DataNumbers.java
@@ -0,0 +1,16 @@
+package com.recruitment.recruitment_task.models;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+
+@Setter
+@Getter
+@RequiredArgsConstructor
+public class DataNumbers {
+
+    private ArrayList<Integer> numbers;
+    private String order;
+}

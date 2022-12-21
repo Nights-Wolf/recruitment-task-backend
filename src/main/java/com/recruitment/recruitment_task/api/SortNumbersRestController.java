ad3578e0c7b357b4c684bc9a89e5ae39fb503ddc
Nights-Wolf <dawinavi@gmail.com>
2022-12-21 17:47:13 +0100

    Cross origin for soritng numbers added.

--- src/main/java/com/recruitment/recruitment_task/api/SortNumbersRestController.java
+++ src/main/java/com/recruitment/recruitment_task/api/SortNumbersRestController.java
@@ -4,14 +4,12 @@ import com.recruitment.recruitment_task.models.DataNumbersRequest;
 import com.recruitment.recruitment_task.servicesInterfaces.SortNumbersServiceInterface;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class SortNumbersRestController {
 
     private final SortNumbersServiceInterface sortNumbersService;
